23f8011b8e45d4e6d24f61a1072ff8cf3c78a418
AnubioG <angela.rubio@genstudents.org>
2024-06-23 17:23:36 -0500

    input validation completed

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 let num;
 let secuencia = [0, 1];
 
-while (isNaN(num) || num <= 1) {
-  num = parseInt(
-    prompt("Ingresa un numero para establecer la secuencia").trim()
-  );
-  for (let i = 0; i < num - 2; i++) {
-    secuencia.push(secuencia[i] + secuencia[i + 1]);
+while (true) {
+  try {
+    num = parseInt(
+      prompt("Ingresa un numero para establecer la secuencia").trim()
+    );
+
+    if (!isNaN(num) || num >= 1) {
+      for (let i = 0; i < num - 2; i++) {
+        secuencia.push(secuencia[i] + secuencia[i + 1]);
+      }
+      alert(`La secuencia fibonacci de ${num} es: ${secuencia}`);
+
+      break;
+    } else {
+      num = parseInt(prompt("Ingresa un numero valido").trim());
+    }
+  } catch (e) {
+    num = parseInt(prompt("Ingresa un numero valido").trim());
   }
-  alert(`La secuencia Fibonacci de ${num} es: ${secuencia}`);
 }
